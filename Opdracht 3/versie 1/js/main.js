a88e71e4662429a7bc033c558f9c363ccf5901fe
Lisa Ottenhof <lisaottenhof@206.159.028.145.hva.nl>
2018-04-10 15:54:57 +0200

    json veranderd

--- Opdracht 3/versie 1/js/main.js
+++ Opdracht 3/versie 1/js/main.js
@@ -2,7 +2,7 @@
 var header = document.querySelector('header');
 var section = document.querySelector('section');
 
-var requestURL = 'https://tomwesterhof.github.io/Frontend-voor-Designers/Opdracht%203/v3/movies.json';
+var requestURL = 'https://tomwesterhof.github.io/Frontend-voor-Designers/Opdracht%203/v3/json/movies.json';
 var request = new XMLHttpRequest();
 
 
